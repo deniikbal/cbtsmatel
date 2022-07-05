1817b5ef1d80ec635b552c0a82b0514349e1301d
deniikbal <deniikbal@gmail.com>
2022-07-05 17:20:10 +0700

    first commit

---
+++ config/config.candy.php
@@ -0,0 +1,9 @@
+<?php
+$copyright = 'Candy CBT';
+
+define("VERSI", "2.9.2");
+define("REVISI", "1");
+define("APLIKASI", "CBT SMATEL");
+define("NAMA_DATABASE", "cbtcandy28");
+define("VERSI_DB", "2.9.2");
+define("BASEPATH", "cbtcandy");
